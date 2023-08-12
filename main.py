4319c63bca13f11ea2730617e53ff7ec106d2b00
GhostLord08 <142110695+GhostLord08@users.noreply.github.com>
2023-08-12 20:05:28 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# Arkadaşlarınızı şaşırtın!
+import random
+
+oyku=["basketbol","badminton","piano","kodlama","coockie","sarı"]
+kullanici=int(input("BENİM HAKKIMDA BİRŞEYLER ÖĞRENMEK İÇİN 1'E BAS...:"))
+if kullanici==1
+    print(random.choice(oyku))
+else:
+    print("ozmn bb")
+    
